e2ea6acc510a2731b559f8606104fdc9374226e2
mareginak <mareginak@gmail.com>
2022-02-16 04:38:39 +0700

    Menambahkan Tugas Kedua Pekan Kedua

---
+++ Tugas-7-Asynchronous/index2.js
@@ -0,0 +1,20 @@
+var readBooksPromise = require('./promise.js')
+ 
+var books = [
+    {name: 'LOTR', timeSpent: 3000}, 
+    {name: 'Fidas', timeSpent: 2000}, 
+    {name: 'Kalkulus', timeSpent: 4000},
+    {name: 'komik', timeSpent: 1000}
+]
+ 
+// Lanjutkan code untuk menjalankan function readBooksPromise
+function startReading(remainingTime, books, index) {
+    readBooksPromise(remainingTime, books[index], function(time) {
+        const nextBook = index + 1;
+        if (nextBook < books.length) {
+            startReading(time, books, nextBook);
+        }
+    });
+}
+
+startReading(10000, books, 0);
